238a47a55e5662f84b407267a3ae9fd638dca28c
WinserEX <winser500@gmail.com>
2023-03-11 14:02:28 -0400

    Improved website structure

--- winser.js
+++ winser.js
@@ -25,4 +25,3 @@ linkT3.setAttribute("crossorigin", "anonymous");
 body.append(linkT3);
 
 
-//<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
