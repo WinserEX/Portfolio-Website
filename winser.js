8c2341f996323af6a204857a89c325393d3e388a
WinserEX <winser500@gmail.com>
2023-03-11 20:33:23 -0400

    Added navbar component to all pages

--- winser.js
+++ winser.js
@@ -1,7 +1,45 @@
+//Components
+let nav = 
+`
+<nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Press</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+`
+
+//Creates Nav container
+let newDiv = document.createElement("div");
+newDiv.setAttribute("class", "navDiv");
+
 //Head tag
 let css = document.querySelector("head");
 //Body tag
 let body = document.querySelector("body");
+
+//Adds navbar to top of body tag
+newDiv.innerHTML = nav;
+body.insertBefore(newDiv, body.firstChild);
 
 //Adds CSS
 let linkT = document.createElement("link");
@@ -18,10 +56,17 @@ linkT2.setAttribute("crossorigin", "anonymous");
 css.append(linkT2);
 
 //Adds bootstrap Js
-let linkT3 = document.createElement("script");
+/* let linkT3 = document.createElement("script");
 linkT3.setAttribute("src", "https://https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js");
 linkT3.setAttribute("integrity", "sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4");
 linkT3.setAttribute("crossorigin", "anonymous");
-body.append(linkT3);
+body.append(linkT3); */
+
+
+
+
+
+
+
 
 
