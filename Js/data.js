040a1d13c22028d0e691e53db534be96ed112a8b
WinserEX <winser500@gmail.com>
2023-03-12 20:36:15 -0400

    Added more skills to data.js

--- Js/data.js
+++ Js/data.js
@@ -6,19 +6,73 @@ let skills = [
         "skill": "JavaScript",
         "img": "../Assets/javascript.png",
         "alt": "JavaScript logo",
-        "learnMore": "https://www.javascript.com/"
+        "learnMore": "../projects.html"
     },
     {
         "skill": "Bootstrap",
         "img": "../Assets/bootstrap.png",
         "alt": "Bootstrap logo",
-        "learnMore": "https://www.javascript.com/"
+        "learnMore": "../projects.html"
     },
     {
         "skill": "React",
         "img": "../Assets/react.png",
         "alt": "React logo",
-        "learnMore": "https://www.javascript.com/"
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "Typescript",
+        "img": "../Assets/typecript.png",
+        "alt": "Typescript logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "Node Js",
+        "img": "../Assets/nodejs.png",
+        "alt": "Node Js logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "Express Js",
+        "img": "../Assets/Expressjs.png",
+        "alt": "Express Js logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "CSS",
+        "img": "../Assets/css.png",
+        "alt": "CSS logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "HTML",
+        "img": "../Assets/html.png",
+        "alt": "HTML logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "SQL",
+        "img": "../Assets/MySQL.png",
+        "alt": "MySQL logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "Mongo DB",
+        "img": "../Assets/MongoDB.png",
+        "alt": "MongoDB logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "Next Js",
+        "img": "../Assets/NextJs.png",
+        "alt": "Next Js logo",
+        "learnMore": "../projects.html"
+    },
+    {
+        "skill": "Wordpress",
+        "img": "../Assets/wordpress.png",
+        "alt": "Wordpress logo",
+        "learnMore": "../projects.html"
     },
 ];
 let publications = [];
