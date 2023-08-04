2d12e7f14d05890b5607b3674cbf724e6338d25f
WinserEX <winser500@gmail.com>
2023-08-03 21:29:52 -0400

    Updated home.html's name, it is now called index.html, also updated home var

--- Js/urls.js
+++ Js/urls.js
@@ -5,7 +5,7 @@ let about = `../about.html`;
 let projects = `../projects.html`;
 let press = `../press.html`;
 let contact = `../contact.html`;
-let home = `../home.html`;
+let home = `../index.html`;
 
 //Social media and contact
 let gitHub = 'https://github.com/WinserEX'
